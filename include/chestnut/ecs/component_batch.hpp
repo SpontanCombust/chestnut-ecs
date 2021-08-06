8114f9360ba44e93719adadf4a2231da9aead9b9
SpontanCombust <cedro_p@o2.pl>
2021-08-06 03:25:07 +0200

    Added component batch with test

---
+++ include/chestnut/ecs/component_batch.hpp
@@ -0,0 +1,83 @@
+#ifndef __CHESTNUT_COMPONENT_BATCH_H__
+#define __CHESTNUT_COMPONENT_BATCH_H__
+
+#include "types.hpp"
+#include "entity_signature.hpp"
+#include "component.hpp"
+#include "component_storage.hpp"
+
+#include <typeindex>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace chestnut
+{
+    /**
+     * @brief A struct-of-arrays containing component pointers belonging to multiple entities having the same component signature
+     * 
+     * @details
+     * A struct housing parallel arrays (vectors) of component pointers. A batch is created for a specified component set signature. 
+     * A struct created from need of storing unspecified number of components of unspecified number of types. 
+     * Allows for convenient access to components that belong to an entity with specific set of components, 
+     * which streamlines component systems' process of fetching very specific component configurations if they need more than one 
+     * type to perform an operation (for example transform component is needed for a renderable component to be drawn at a specific location). 
+     * You may submit component by passing a component set with corresponding signature. 
+     * You may get component pointers in form of a vector for specified component type. 
+     */
+    struct SComponentBatch
+    {
+        CEntitySignature signature;
+        
+        std::vector< entityid > vecEntityIDs;
+        std::unordered_map< std::type_index, std::vector< CComponent * > > mapCompTypeToCompVec;
+    };
+
+
+    /**
+     * @brief A helper class for managing component batches
+     * 
+     * With access to CComponentStorageTypeMap this class can manage the actual component batch they store
+     * by fetching appropriate sets of components for different entities and laying them out in the batch
+     * in the coherent manner, such that you can traverse vectors in mapCompTypeToCompVec as if they are
+     * arrays in a typical SOA structure, except here it's not a actual struct, it's just a map of types
+     */
+    class CComponentBatchGuard
+    {
+    private:
+        // used to add entity index information to target batch
+        // order matters, because we'll use this vector's indices to modify vectors in the map below
+        std::vector< entityid > m_pendingIn_vecEntityIDs;
+        // used to add component data to target batch
+        std::unordered_map< std::type_index, std::vector< CComponent * > > m_pendingIn_mapCompTypeToVecComp;
+
+        // used to remove component data from target batch; we remove whole component row by querying indices with these owner IDs
+        // it's a set so we can find IDs quicker; order doesn't matter here
+        std::unordered_set< entityid > m_pendingOut_setEntityIDs;
+
+
+        SComponentBatch m_targetBatch;
+
+    public:
+        CComponentBatchGuard( const CEntitySignature& signature );
+
+
+        // You have to make sure the entity in question has all required components
+        // i.e. components specified by batch signature
+        // Doesn't check for duplicates
+        void fetchAndAddEntityWithComponents( entityid entityID, const CComponentStorageTypeMap& storageMap );
+
+        void removeEntityWithComponents( entityid entityID );
+
+        void updateBatch();
+
+
+        const CEntitySignature& getBatchSignature() const;
+
+        const SComponentBatch *getBatchPtr() const;
+    };
+    
+    
+} // namespace chestnut
+
+#endif // __CHESTNUT_COMPONENT_BATCH_H__
---
+++ src/chestnut/ecs/component_batch.cpp
@@ -0,0 +1,157 @@
+#include "chestnut/ecs/component_batch.hpp"
+
+#include <algorithm>
+
+namespace chestnut
+{    
+    CComponentBatchGuard::CComponentBatchGuard( const CEntitySignature& signature ) 
+    {
+        m_targetBatch.signature = signature;
+
+        // initialize batch's component map
+        for( const std::type_index& type : signature.m_setComponentTindices )
+        {
+            m_targetBatch.mapCompTypeToCompVec[ type ] = std::vector< CComponent * >();
+        }
+    }
+
+    void CComponentBatchGuard::fetchAndAddEntityWithComponents( entityid entityID, const CComponentStorageTypeMap& storageMap ) 
+    {
+        // First of all check if entity has been scheduled for removal
+        // In that case only delete removal data
+        // We can assume entity data already was in the batch before scheduled removal
+        // Don't allow stuff to be removed in the first place if it is to be added back in
+
+        auto it = m_pendingOut_setEntityIDs.find( entityID );
+
+        if( it != m_pendingOut_setEntityIDs.end() )
+        {
+            m_pendingOut_setEntityIDs.erase( it );
+        }
+        // Otherwise fetch and add data
+        else
+        {
+            m_pendingIn_vecEntityIDs.push_back( entityID );
+
+            for( const std::type_index& type : m_targetBatch.signature.m_setComponentTindices )
+            {
+                // This ~~shouldn't~~ throw an exception if systems using this class are setup correctly
+                IComponentStorage *typedStorage = storageMap.at( type );
+
+                CComponent *comp = typedStorage->getComponent( entityID );
+
+                m_pendingIn_mapCompTypeToVecComp[ type ].push_back( comp );       
+            } 
+        }
+    }
+
+    void CComponentBatchGuard::removeEntityWithComponents( entityid entityID ) 
+    {
+        // First check if entity has been scheduled for addition
+        // In that case delete addition data
+        // Don't allow stuff to be added in the first place if it is to be removed later on anyways
+
+        // do a for loop search, because we'll need the index
+        entityid i;
+        for (i = 0; i < m_pendingIn_vecEntityIDs.size(); i++)
+        {
+            if( m_pendingIn_vecEntityIDs[i] == entityID )
+            {
+                break;
+            }
+        }
+
+        if( i < m_pendingIn_vecEntityIDs.size() )
+        {
+            // use std::next to erase using index
+            m_pendingIn_vecEntityIDs.erase( std::next( m_pendingIn_vecEntityIDs.begin(), i ) );
+
+            for( auto& [ type, vecComp ] : m_pendingIn_mapCompTypeToVecComp )
+            {
+                // components in these mapped vectors are at the same index as they are
+                // in the vector of entity IDs, because we add them in the same order
+                vecComp.erase( std::next( vecComp.begin(), i ) );
+            }
+        }
+        // if the entity wasn't to be added, then maybe it already is in the batch - schedule it for removal
+        else
+        {
+            m_pendingOut_setEntityIDs.insert( entityID );
+        }
+    }
+
+    void CComponentBatchGuard::updateBatch() 
+    {
+        // first do the removal
+
+        // iterate over all IDs in the target batch vector of entity IDs
+        // remove those that are in the set of IDs sheduled for removal 
+
+        std::vector< entityid > vecIndicesToEraseInOrder;
+
+        for (entityid i = 0; i < m_targetBatch.vecEntityIDs.size(); /*NOP*/)
+        {
+            entityid entityID = m_targetBatch.vecEntityIDs[i];
+
+            if( m_pendingOut_setEntityIDs.find( entityID ) != m_pendingOut_setEntityIDs.end() )
+            {
+                m_targetBatch.vecEntityIDs.erase( std::next( m_targetBatch.vecEntityIDs.begin(), i ) );
+                vecIndicesToEraseInOrder.push_back(i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        for( auto& [ type, vecComp ] : m_targetBatch.mapCompTypeToCompVec )
+        {
+            for( entityid index : vecIndicesToEraseInOrder )
+            {
+                vecComp.erase( std::next( vecComp.begin(), index ) );
+            }
+        }
+        
+
+
+        // then addition
+
+        // add to target batch all the IDs of entities of pending components
+        m_targetBatch.vecEntityIDs.insert( m_targetBatch.vecEntityIDs.end(),
+                                           m_pendingIn_vecEntityIDs.begin(),
+                                           m_pendingIn_vecEntityIDs.end() );
+
+        // now add the actual components to target batch
+        for( auto& [ type, vecComp ] : m_targetBatch.mapCompTypeToCompVec )
+        {
+            vecComp.insert( vecComp.end(),
+                            m_pendingIn_mapCompTypeToVecComp[ type ].begin(), 
+                            m_pendingIn_mapCompTypeToVecComp[ type ].end() );
+        }
+
+
+
+        // clear pending data
+
+        m_pendingOut_setEntityIDs.clear();
+
+        m_pendingIn_vecEntityIDs.clear();
+
+        for( auto& [ type, vecComp ] : m_pendingIn_mapCompTypeToVecComp )
+        {
+            vecComp.clear();
+        }
+    }
+
+    const CEntitySignature& CComponentBatchGuard::getBatchSignature() const
+    {
+        return m_targetBatch.signature;
+    }
+
+    const SComponentBatch* CComponentBatchGuard::getBatchPtr() const
+    {
+        return &m_targetBatch;
+    }
+
+
+} // namespace chestnut
