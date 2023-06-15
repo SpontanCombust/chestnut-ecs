3344611815780e28d44549f1918cc605445e36c3
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-06-15 15:10:36 +0200

    FEAT: deferred entity world commands

---
+++ include/chestnut/ecs/command_queue.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+#include <vector>
+
+#include "command.hpp"
+
+namespace chestnut::ecs
+{
+    class CEntityWorld;
+
+    class CCommandQueue
+    {
+    private:
+        std::vector<std::byte> m_buff;
+
+    public:
+        template<typename C>
+        void insert(C &&cmd)
+        {
+            static_assert(std::is_base_of_v<ICommand, C>, "The command class should implement ICommand interface");
+
+            size_t cmdSize = cmd.size();
+
+            // make room for the new command
+            m_buff.resize(m_buff.size() + cmdSize);
+
+            // pointer to the beginning of the buffer space for the new command
+            std::byte *eob = m_buff.data() + m_buff.size() - cmdSize;
+
+            // use placement new to safely move the command and its contents to the buffer
+            new (eob) C(std::move(cmd));
+        }
+
+        void execute(CEntityWorld& world)
+        {
+            if(m_buff.empty())
+            {
+                return;
+            }
+
+            for(size_t i = 0; i < m_buff.size();)
+            {
+                std::byte *data = &m_buff[i];
+                ICommand *cmd = (ICommand *)data;
+                size_t cmdSize = cmd->size();
+
+                cmd->excecute(world);
+
+                i += cmdSize;
+            }
+        }
+    };
+
+} // namespace chetnut::ecs
---
+++ include/chestnut/ecs/commands.hpp
@@ -0,0 +1,217 @@
+#pragma once
+
+#include "command.hpp"
+#include "entity_world.hpp"
+#include "command_queue.hpp"
+
+#include <tuple>
+
+namespace chestnut::ecs
+{
+    template<class ...Cs>
+    class CCreateEntityCommand;
+    
+    template<>
+    class CCreateEntityCommand<> : public ICommand
+    {
+    private:
+        bool m_canRecycleId;
+
+    public:
+        CCreateEntityCommand(bool canRecycleId = true)
+        : m_canRecycleId(canRecycleId)
+        {
+
+        }
+
+        void excecute(CEntityWorld& world) override
+        {
+            world.createEntity(m_canRecycleId);
+        }
+
+        size_t size() const override
+        {
+            return sizeof(CCreateEntityCommand<>);
+        }
+    };
+
+    template<class C, class... CRest>
+    class CCreateEntityCommand<C, CRest...> : public ICommand
+    {
+    private:
+        using TupleType = std::tuple<C, CRest...>;
+        TupleType m_data;
+        bool m_canRecycleId;
+        
+    public: 
+        CCreateEntityCommand(TupleType &&data, bool canRecycleId = true)
+        : m_data(std::move(data)), m_canRecycleId(canRecycleId)
+        {
+
+        }
+
+        void excecute(CEntityWorld& world) override
+        {
+            world.createEntityWithComponents(std::move(m_data), m_canRecycleId);
+        }
+
+        size_t size() const override
+        {
+            return sizeof(CCreateEntityCommand<C, CRest...>);
+        }
+    };
+
+
+
+    class IEntityCommand : public ICommand
+    {
+    protected:
+        entityid_t m_entityId;
+
+    public:
+        IEntityCommand(entityid_t entityId) 
+        : m_entityId(entityId) 
+        {
+
+        }
+
+        entityid_t entity() const
+        {
+            return m_entityId;
+        }
+    };
+
+    class CDestroyEntityCommand : public IEntityCommand
+    {
+    public:
+        CDestroyEntityCommand(entityid_t entityId)
+        : IEntityCommand(entityId)
+        {
+
+        }
+
+        void excecute(CEntityWorld& world) override
+        {
+            world.destroyEntity(m_entityId);
+        }
+
+        size_t size() const override
+        {
+            return sizeof(CDestroyEntityCommand);
+        }
+    };
+
+
+
+    template<class C>
+    class CCreateOrUpdateComponentCommand : public IEntityCommand
+    {
+    private:
+        C m_data;
+
+    public:
+        CCreateOrUpdateComponentCommand(entityid_t id, C &&data)
+        : IEntityCommand(id), m_data(data)
+        {
+
+        }
+
+        void excecute(CEntityWorld& world) override
+        {
+            world.createOrUpdateComponent(m_entityId, std::move(m_data));
+        }
+
+        size_t size() const override
+        {
+            return sizeof(CCreateOrUpdateComponentCommand);
+        }
+    };
+
+    template<class C>
+    class CDestroyComponentCommand : public IEntityCommand
+    {
+    public:
+        CDestroyComponentCommand(entityid_t id)
+        : IEntityCommand(id)
+        {
+
+        }
+
+        void excecute(CEntityWorld& world) override
+        {
+            world.destroyComponent<C>(m_entityId);
+        }
+
+        size_t size() const override
+        {
+            return sizeof(CDestroyComponentCommand);
+        }
+    };
+
+
+
+    class CCommands
+    {
+    private:
+        CCommandQueue m_queue;
+
+    public:
+        template<typename... Cs>
+        CCommands& createEntity(Cs&&... data)
+        {
+            using TupleType = std::tuple<Cs...>;
+            TupleType t = std::make_tuple(data...);
+            m_queue.insert(CCreateEntityCommand<Cs...>(std::forward<TupleType>(t), true));
+            return *this;
+        }
+
+        CCommands& createEntity()
+        {
+            m_queue.insert(CCreateEntityCommand<>(true));
+            return *this;
+        }
+
+        template<typename... Cs>
+        CCommands& createUniqueEntity(Cs&&... data)
+        {
+            using TupleType = std::tuple<Cs...>;
+            TupleType t = std::make_tuple(data...);
+            m_queue.insert(CCreateEntityCommand<Cs...>(std::forward<TupleType>(t), false));
+            return *this;
+        }
+
+        CCommands& createUniqueEntity()
+        {
+            m_queue.insert(CCreateEntityCommand<>(false));
+            return *this;
+        }
+
+        CCommands& destroyEntity(entityid_t ent)
+        {
+            m_queue.insert(CDestroyEntityCommand(ent));
+            return *this;
+        }
+
+        template<class C>
+        CCommands& createOrUpdateComponent(entityid_t ent, C&& data)
+        {
+            m_queue.insert(CCreateOrUpdateComponentCommand(ent, std::forward<C>(data)));
+            return *this;
+        }
+
+        template<class C>
+        CCommands& destroyComponent(entityid_t ent)
+        {
+            m_queue.insert(CDestroyComponentCommand<C>(ent));
+            return *this;
+        }
+
+
+        CCommandQueue& getCommandQueue()
+        {
+            return m_queue;
+        }
+    };  
+
+
+} // namespace chestnut::ecs
